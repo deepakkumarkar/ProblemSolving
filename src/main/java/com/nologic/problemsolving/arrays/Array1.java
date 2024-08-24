bdd366eef4054b360865c552f998bd5dfd4cd498
deepakkumarkar <dk.kar369@gmail.com>
2024-08-25 01:55:10 +0530

    converting into maven projct

--- src/main/java/com/nologic/problemsolving/arrays/Array1.java
+++ src/main/java/com/nologic/problemsolving/arrays/Array1.java
@@ -1,3 +1,4 @@
+package com.nologic.problemsolving.arrays;
 
 import java.util.*;
 
@@ -6,7 +7,7 @@ import java.util.*;
  * @author deepakkar
  * program to find the max and min in an array
  */
-class Array {
+class Array1 {
 	public static void main(String[] args) {
 		int a[] = { 1, 423, 6, 46, 34, 23, 13, 53, 4 };
 
@@ -17,4 +18,5 @@ class Array {
 		// nth position will be maximum
 		System.out.println("min-" + a[0] + " max-" + a[a.length - 1]);
 	}
+	
 }
